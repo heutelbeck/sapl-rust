9a6b984b53d1d48186d642a7cee48db4b5eebf3a
Stefan Weng <stefan.weng@gmx.net>
2025-08-23 16:59:33 +0200

    modify sapl-server-rs launch function to be able to write unit tests for endpoints and add first unit tests for endpoint decide-once

--- sapl-server-rs/src/main.rs
+++ sapl-server-rs/src/main.rs
@@ -16,7 +16,7 @@
 
 use embedded_pdp::{AuthorizationSubscription, Pdp, file_reader};
 use rocket::{
-    Orbit, Rocket,
+    Build, Orbit, Rocket,
     fairing::{Fairing, Info, Kind},
     response::stream::{Event, EventStream},
     serde::json::{Json, Value, json},
@@ -27,6 +27,7 @@ use std::path::Path;
 #[macro_use]
 extern crate rocket;
 
+#[allow(dead_code)]
 struct FileWatcher;
 
 #[rocket::async_trait]
@@ -74,14 +75,117 @@ fn health() -> Value {
 }
 
 #[launch]
-fn entry_point() -> _ {
+fn rocket() -> Rocket<Build> {
     let pdp = Pdp::new(
         Some(Path::new("policies/pdp.json")),
         Some(Path::new("policies/")),
     );
 
-    rocket::build()
-        .manage(pdp)
-        .attach(FileWatcher)
-        .mount("/", routes![decide, decide_once, health])
+    #[allow(unused_mut)]
+    let mut rocket_builder = rocket::build().manage(pdp);
+
+    // FileWatcher only in production used and not in tests
+    #[cfg(not(test))]
+    {
+        rocket_builder = rocket_builder.attach(FileWatcher);
+    }
+
+    rocket_builder.mount("/", routes![decide, decide_once, health])
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use rocket::http::{ContentType, Status};
+    use rocket::local::blocking::Client;
+
+    #[test]
+    fn health() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let response = client.get(uri!(health())).dispatch();
+        assert_eq!(response.status(), Status::Ok);
+
+        assert_eq!(
+            response.into_string().unwrap(),
+            "{\"status\":\"up\",\"version\":\"0.1.0\"}"
+        );
+    }
+
+    #[test]
+    fn decide_once_status_not_found() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let response = client.post(uri!(decide_once())).dispatch();
+        assert_eq!(response.status(), Status::NotFound);
+    }
+
+    #[test]
+    fn decide_once_deny() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let response = client
+            .post(uri!(decide_once()))
+            .header(ContentType::JSON)
+            .body("{\"subject\":\"WILLI\",\"action\":\"write\", \"resource\":\"something\"}")
+            .dispatch();
+        assert_eq!(response.status(), Status::Ok);
+
+        assert_eq!(response.into_string().unwrap(), "{\"decision\":\"DENY\"}");
+    }
+
+    #[test]
+    fn decide_once_permit_policy2() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let response = client
+            .post(uri!(decide_once()))
+            .header(ContentType::JSON)
+            .body("{\"subject\":\"WILLI\",\"action\": {\"java\": { \"name\": \"findById\"}}, \"resource\":\"something\"}")
+            .dispatch();
+        assert_eq!(response.status(), Status::Ok);
+
+        assert_eq!(response.into_string().unwrap(), "{\"decision\":\"PERMIT\"}");
+    }
+
+    #[test]
+    fn decide_once_deny_simpsons_policy_set() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let response = client
+            .post(uri!(decide_once()))
+            .header(ContentType::JSON)
+            .body("{\"subject\":\"bs@simpsons.com\",\"action\": \"read\", \"resource\":\"something\"}")
+            .dispatch();
+        assert_eq!(response.status(), Status::Ok);
+
+        assert_eq!(response.into_string().unwrap(), "{\"decision\":\"DENY\"}");
+    }
+
+    #[test]
+    fn decide_once_permit_transform_demo_set() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let response = client
+            .post(uri!(decide_once()))
+            .header(ContentType::JSON)
+            .body("{\"subject\":{\"name\": \"anonymous\"},\"action\": {\"http\": { \"contextPath\": \"/changedstring\", \"requestedURI\": \"http://localhost:8080/numbers\"}}, \"resource\":\"something\"}")
+            .dispatch();
+        assert_eq!(response.status(), Status::Ok);
+
+        assert_eq!(
+            response.into_string().unwrap(),
+            "{\"decision\":\"PERMIT\",\"resource\":\"***something***\",\"obligation\":{\"type\":\"logAccess\",\"message\":\"User anonymous has accessed: http://localhost:8080/numbers\"}}"
+        );
+    }
+
+    #[test]
+    fn decide_once_permit_obligation_advice_demo_set() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let response = client
+            .post(uri!(decide_once()))
+            .header(ContentType::JSON)
+            .body("{\"subject\":{\"name\": \"anonymous\"},\"action\": {\"http\": { \"contextPath\": \"/numbers\", \"requestedURI\": \"http://localhost:8080/numbers\"}}, \"resource\":\"something\"}")
+            .dispatch();
+        assert_eq!(response.status(), Status::Ok);
+
+        assert_eq!(
+            response.into_string().unwrap(),
+            "{\"decision\":\"PERMIT\",\"obligation\":{\"type\":\"sendEmail\",\"recipient\":\"Adam Admin <admin@example.com>\",\"subject\":\"Data was accessed.\",\"message\":\"Administrator anonymous has accessed: http://localhost:8080/numbers\"},\"advice\":{\"type\":\"logAccess\",\"message\":\"User anonymous has accessed: http://localhost:8080/numbers\"}}"
+        );
+    }
 }
