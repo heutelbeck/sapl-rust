674f117a86db736d09b033dc20970b59cc4f5633
Stefan Weng <stefan.weng@gmx.net>
2025-08-29 17:28:57 +0200

    add benchmark test in embedded-pdp for decide-once function

--- embedded-pdp/README.md
+++ embedded-pdp/README.md
@@ -5,3 +5,19 @@ Since this implementation cannot be accessed via a network connection, it is ref
 ## How it works
 
 ## Core Components
+
+
+## Performance Analysis
+One approach to identifying negative effects on performance when changes are made to the code is to measure the execution speed. The crate [Criterion.rs](https://crates.io/crates/criterion) is used to detect and measure performance improvements or regressions.
+
+
+This is possible with the benchmark test found at `benches/decide_once_benchmark.rs`. The decide_once method is measured. The measurement can be performed with the following command:
+
+```
+cargo bench
+```
+
+The result is displayed immediately after the measurement. A detailed report is stored in the `target` folder of the workspace. You can find a sample report [here](benchmark_report_example/criterion/decide_once/report/index.html).
+
+> [!NOTE]
+> In order to be able to create the graphics for the report, `gnuplot` must be installed.
---
+++ embedded-pdp/benches/decide_once_benchmark.rs
@@ -0,0 +1,41 @@
+/*
+    Copyright 2025 Stefan Weng
+
+    Licensed under the Apache License, Version 2.0 (the "License"); you may not
+    use this file except in compliance with the License. You may obtain a copy
+    of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+    License for the specific language governing permissions and limitations
+    under the License.
+*/
+
+use criterion::{Criterion, criterion_group, criterion_main};
+use embedded_pdp::{AuthorizationSubscription, Pdp};
+use std::hint::black_box;
+use std::path::Path;
+
+fn criterion_decide_once_benchmark(c: &mut Criterion) {
+    let pdp = Pdp::new(
+        Some(Path::new(
+            "/home/stefan/Dokumente/uni/Thesis/sapl-rust/sapl-server-rs/policies/pdp.json",
+        )),
+        Some(Path::new(
+            "/home/stefan/Dokumente/uni/Thesis/sapl-rust/sapl-server-rs/policies/",
+        )),
+    );
+
+    c.bench_function("decide_once", |b| {
+        b.iter(|| {
+            let auth_sub = AuthorizationSubscription::new_example_subscription1();
+            black_box(pdp.decide_once(black_box(auth_sub)))
+        })
+    });
+}
+
+criterion_group!(benches, criterion_decide_once_benchmark);
+criterion_main!(benches);
